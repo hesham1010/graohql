4d14730b76221fc2a2953d9d52c4208a24cbb698
hesham1010 <hesham.gublootech@gmail.com>
2024-05-17 22:55:24 +0500

    changes

---
+++ server/Schema/schema.js
@@ -0,0 +1,48 @@
+const { projects, clients } = require("../sampleData");
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList,
+} = require("graphql");
+// VCL
+const ClientType = new GraphQLObjectType({
+  name: "Client",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
+const ProjectType = new GraphQLObjectType({
+  name: "Client",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
+const RootQuery = new GraphQLObjectType({
+  name: "ROOTQueryType",
+  fields: {
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        return clients;
+      },
+    },
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return clients.find((client) => client.id === args.id);
+      },
+    },
+  },
+});
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
